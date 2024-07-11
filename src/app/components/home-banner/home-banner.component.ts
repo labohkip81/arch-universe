bd98aef72f9cad80a20057b6a0448085aa9de677
Labohkip81 <labankiplagat81@gmail.com>
2024-07-11 19:07:59 +0300

    Homepage design

--- src/app/components/home-banner/home-banner.component.ts
+++ src/app/components/home-banner/home-banner.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DayNightService } from '../../services/day-night.service';
+import { SwiperOptions } from 'swiper/types';
 
 @Component({
   selector: 'app-home-banner',
@@ -7,6 +8,18 @@ import { DayNightService } from '../../services/day-night.service';
   styleUrl: './home-banner.component.css'
 })
 export class HomeBannerComponent {
+
+  // Swiper
+  swiperConfig: SwiperOptions = {
+    // spaceBetween: 10,
+    // navigation: true,
+    // preventClicks: false,
+    // slidesPerView: 'auto',
+    // freeMode: true,
+    // watchSlidesProgress: true,
+    // noSwipingSelector: 'a',
+    // focusableElements: 'input, select, option, textarea, video, label'
+  }
 
   constructor(private dayNightService: DayNightService){
 
